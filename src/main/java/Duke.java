265d365dc55b240b74c48f4d5cdf469765333abb
Lucas Foo <lucasfoo33@gmail.com>
2019-08-18 17:02:05 +0800

    Week 2 Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static List<String> taskList = new ArrayList<>();
+    private static List<Task> taskList = new ArrayList<>();
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -29,22 +29,34 @@ public class Duke {
 
     private static void input(){ Scanner scanner = new Scanner(System.in);
         String inputLine = scanner.nextLine();
-        if(inputLine.equals("bye")){
+        String inputList[] =  inputLine.split(" ");
+        if(inputList[0].equals("done")) {
+            int listNum = Integer.parseInt(inputList[1]);
+            Task currTask = taskList.get(listNum - 1);
+            currTask.markDone();
+            taskList.set(listNum - 1, currTask);
+            print_line();
+            System.out.println(" Nice! I've marked this task as done:\n" + "  [" + currTask.getStatusIcon() + "] " + currTask.description);
+            print_line();
+            input();
+        }else if(inputLine.equals("bye")){
             print_line();
             System.out.println("Bye. Hope to see you again soon!");
             print_line();
             System.exit(0);
         }else if(inputLine.equals("list")){
             print_line();
+            System.out.println("Here are the tasks in your list:");
             int counter = 1;
-            for (String pastTasks : taskList){
-                System.out.println(counter + ". " + pastTasks);
+            for (Task pastTasks : taskList){
+                System.out.println(counter + ".[" + pastTasks.getStatusIcon() + "] " + pastTasks.description);
                 ++counter;
             }
             print_line();
             input();
         }else{
-            taskList.add(inputLine);
+            Task task = new Task(inputLine);
+            taskList.add(task);
             print_line();
             System.out.println("added: " + inputLine);
             print_line();
